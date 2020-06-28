ac2fc73db25aa91ae20e6cacb8c03549a323aac1
Ignacio Rojas Aguilar <imoon521@gmail.com>
2020-06-27 23:20:50 -0600

    Cleaning placeholder app

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -6,8 +6,5 @@ describe('client', () => {
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to client!');
   });
 });
